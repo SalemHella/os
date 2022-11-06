87b1ec889defd0f27ae8116083b8af7a38490ab8
SalemHella <salem_hellal@protonmail.com>
2022-11-06 13:40:38 +0100

    fist commit

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World";
+
 #[no_mangle]
 
 pub extern "C" fn _start() -> ! {
+    let vag_buffer = 0xb8000 as *mut u8;
+    for (i,&byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vag_buffer.offset(i as isize * 2) = byte;
+            *vag_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop{}
 } 
